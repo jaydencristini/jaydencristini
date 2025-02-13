0a970cea23d5af860d2c3472fb72b46565090376
jaydencristini <jayden.cristini@gmail.com>
2025-02-12 22:49:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+var i = 0;
+var txt = 'Download Resume';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("typewriter").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+}
+    */
